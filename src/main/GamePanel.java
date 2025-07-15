735c3d2f4b837cfe9a1657eccdd02a9f247cc110
sena <gulozseven@gmail.com>
2025-07-16 01:02:46 +0300

    Game Loop (sleep) & Key Input (WSAD)
    might change the game loop method to "delta" --> more "accurate" in milli's... i think?

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,8 +14,17 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; //48*16=768 pixels
     final int screenHeight = tileSize * maxScreenRow; //48*12=576 pixels
 
+    // FPS!!
+    int FPS = 60;
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;//run program until you stop it
+
+
+    //set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
 
     //first constructor
@@ -26,6 +35,9 @@ public class GamePanel extends JPanel implements Runnable {
         //if set to true, all the drawing from this component...
         //...will be done in an offscreen painting buffer...
         //...to enhance game's rendering performance.
+
+        this.addKeyListener(keyH); //so the game panel recognizes the key input
+        this.setFocusable(true); //w this, game panel can be 'focused' to receive key input.
 
     }
 
@@ -39,14 +51,53 @@ public class GamePanel extends JPanel implements Runnable {
     public void run() {
         //game loop --codes that executes in every 'frame' in the fps
         while (gameThread != null) {
+            // NOTE:
+            // long currentTime = System.nanoTime();
+            //--> returns the current value of the running
+            // JVM's high resolution time source - in nanoseconds. 1mil nanosec = 1 sec
+
+            //SLEEP STRATEGY -- GAME LOOP
+            double drawInterval = (double) 1000000000 / FPS; //0.01666 seconds --> THE ALLOCATED TIME FOR A LOOP
+            double nextDrawTime = System.nanoTime() + drawInterval;
+
+
             //1. update information (such as character positions)
             update();
             //2. draw the screen with the updated information
             repaint();
+
+            //remaining time AFTER the program executes update() and repaint() in the given time
+            //e.g. in our game, the allocated time is 0.01666 seconds.
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000; //sleep method only accepts milliseconds, so im converting from nano to milli
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime); //--> sleep pauses the game loop for the remaining time.
+
+                nextDrawTime += drawInterval; //0.01666 seconds (one drawInterval) later will be the NEXT DRAW TIME
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-    public void update() {
 
+    //UPDATES the ucubik's coordinates
+    public void update() {
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;   //--> left corner is (0,0) so, y values increases as they go DOWN...
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;   //--> ...and increases as they go RIGHT
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
     }
 
     //built-in method
@@ -54,7 +105,7 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         //to save some memories...
         g2.dispose(); //dispose of this graphics context and release any system resources that is using it.
